9102c282e7a5326934b35f9fa413d292cd43f03d
Johnatan Ruiz Campos <je_blue87@hotmail.com>
2021-06-13 11:02:38 +0000

    v1.9.3

--- pipeline/PIPELINE-FULL-STAGING/2-testing.sh
+++ pipeline/PIPELINE-FULL-STAGING/2-testing.sh
@@ -3,30 +3,37 @@
 source py37-venv/bin/activate
 set -x
 
-#a) Prueba de Revisión estática de Código con radon
+# a) Prueba de Revisión estática de Código con radon
 radon cc src -a -nc
-echo 'Resultado: '.$?
+# echo 'Resultado: '.$?
 
 if [[ $? -ne 0 ]]
 then
-	echo '-Falló el test de revisión estática de código con Radon'
+	echo '-Falló el test de revisión estática de código con radon'
 	exit 1
+else 
+	echo echo '-El test de Revisión estática de código con radon terminó correctamente'
 fi
 
-#b) Prueba de Calidad de Código con flake8
+# b) Prueba de Calidad de Código con flake8
 flake8 src
 if [[ $? -ne 0 ]]
 then
-	echo '-Falló el test de Calidad de código con Flake8'
+	echo '-Falló el test de Calidad de código con flake8'
 	exit 1
+else 
+	echo echo '-El test de Calidad de código con flake8 terminó correctamente'
 fi
 
-#c) Prueba de Seguridad en el Código con bandit
-#bandit -r src
-#if [[ $? -ne 0 ]]
-#then
-#	exit 1
-#fi
+# c) Prueba de Seguridad en el Código con bandit
+bandit -r src
+if [[ $? -ne 0 ]]
+then
+	echo '-Falló el test de Seguridad en el código con bandit'
+	exit 1
+else 
+	echo echo '-El test de Seguridad en el código con bandit terminó correctamente'
+fi
 
 cd tests/unit/
 #coverage run TestToDoTableClass.py
