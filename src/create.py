9e788c2c9eb8561a005eb7098e9f55a547b7ea9a
Johnatan Ruiz Campos <je_blue87@hotmail.com>
2021-06-13 10:21:47 +0000

    v1.8.5

--- src/create.py
+++ src/create.py
@@ -1,11 +1,11 @@
 import json
 import logging
 import os
-import time
-import uuid
+# import time
+# import uuid
 import boto3
 
-from utils import decimalencoder
+# from utils import decimalencoder
 from utils.todoTableClass import todoTableClass
 
 
@@ -14,21 +14,19 @@ if os.environ["ENVIRONMENT"] == "LOCAL":
 else:
     dynamodb = boto3.resource("dynamodb")
 
-
-#Agrega un nuevo elemento a la lista
+# Agrega un nuevo elemento a la lista
 def create(event, context):
     data = json.loads(event['body'])
-    
+
     if 'text' not in data:
         logging.error("Validation Failed")
         raise Exception("Couldn't create the todo item.")
     
     mytable = todoTableClass(dynamodb)
     item = mytable.put_todo(data['text'])
-    
+
     response = {
                 "statusCode": 200,
                 "body": json.dumps(item)
             }
-
     return response
