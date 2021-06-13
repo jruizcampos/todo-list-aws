95ffb92ec81202c9a5111b96282a6b026419163a
Johnatan Ruiz Campos <je_blue87@hotmail.com>
2021-06-13 10:49:19 +0000

    v1.9.1

--- src/utils/todoTableClass.py
+++ src/utils/todoTableClass.py
@@ -1,24 +1,24 @@
-import json
-import logging
+# import json
+# import logging
 import os
 import time
 import uuid
 import boto3
 
-from utils import decimalencoder
+# from utils import decimalencoder
 traductor = boto3.client('translate')
 
 
 class todoTableClass(object):
 
-    #def __init__(self, table, dynamodb=None):
+    # def __init__(self, table, dynamodb=None):
     def __init__(self, dynamodb=None):
-        #self.tableName = table
+        # self.tableName = table
         self.tableName = os.environ['DYNAMODB_TABLE']
-        
+
         if not dynamodb:
-            dynamodb = boto3.resource('dynamodb', endpoint_url='http://dynamodb:8000') 
-			#dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
+            dynamodb = boto3.resource('dynamodb', endpoint_url='http://dynamodb:8000')
+            # dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
         self.dynamodb = dynamodb
 
     def create_todo_table(self):
@@ -54,10 +54,10 @@ class todoTableClass(object):
         table = self.dynamodb.Table(self.tableName)
         table.delete()
 
-    #Obtiene un elemento de la tabla a partir de su id
+    # Obtiene un elemento de la tabla a partir de su id
     def get_todo(self, id):
         table = self.dynamodb.Table(self.tableName)
-        
+
         try:
             result = table.get_item(
                 Key={
@@ -69,22 +69,22 @@ class todoTableClass(object):
         else:
             return result
 
-    #Obtiene la lista completa de elementos
+    # Obtiene la lista completa de elementos
     def scan_todo(self):
         table = self.dynamodb.Table(self.tableName)
-        
+
         try:
             result = table.scan()
         except Exception as e:
             raise e
         else:
             return result
-    
-    #Actualiza un elemento de la tabla a partir de su id
+
+    # Actualiza un elemento de la tabla a partir de su id
     def update_todo(self, text, id, checked):
         timestamp = int(time.time() * 1000)
         table = self.dynamodb.Table(self.tableName)
-        
+  
         try:
             result = table.update_item(
                 Key={
@@ -108,14 +108,14 @@ class todoTableClass(object):
         else:
             return result
 
-    #Agrega un nuevo elemento a la lista
+    # Agrega un nuevo elemento a la lista
     def put_todo(self, text, id=None):
         if not id:
             id = str(uuid.uuid1())
-        
+
         timestamp = str(time.time())
         table = self.dynamodb.Table(self.tableName)
-        
+
         try:
             item = {
                 'id': id,
@@ -131,10 +131,10 @@ class todoTableClass(object):
         else:
             return item
 
-    #Elimina un elemento de la lista
+    # Elimina un elemento de la lista
     def delete_todo(self, id):
         table = self.dynamodb.Table(self.tableName)
-        
+
         try:
             table.delete_item(
                 Key={
@@ -145,20 +145,22 @@ class todoTableClass(object):
             raise e
         else:
             return True
-    
-    #Traduce un elemento de la lista
+
+    # Traduce un elemento de la lista
     def translate_todo(self, id, source_language, target_language):
         table = self.dynamodb.Table(self.tableName)
-        
+
         result = table.get_item(
             Key={
                 'id': id
             }
         )
-        
+
         try:
-            result = traductor.translate_text(Text=result['Item'].get('text'), SourceLanguageCode=source_language, TargetLanguageCode=target_language)
-        
+            result = traductor.translate_text(Text=result['Item'].get('text'),
+                                              SourceLanguageCode=source_language,
+                                              TargetLanguageCode=target_language)
+
         except Exception as e:
             raise e
         else:
