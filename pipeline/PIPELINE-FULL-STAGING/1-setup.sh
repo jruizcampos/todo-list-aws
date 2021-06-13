23da25bf04c744c7e4d6326fdac85050b517d76d
Johnatan Ruiz Campos <je_blue87@hotmail.com>
2021-06-13 07:51:30 +0000

    v1.7.4

--- pipeline/PIPELINE-FULL-STAGING/1-setup.sh
+++ pipeline/PIPELINE-FULL-STAGING/1-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run -d --name dynamodb -p 8000:8000 amazon/dynamodb-local
 sleep 10
-aws dynamodb create-table --table-name localTable --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH 
+aws dynamodb create-table --table-name localTable --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
 python3.7 -m venv py37-venv
 source py37-venv/bin/activate
 pip install -r requirements.txt
