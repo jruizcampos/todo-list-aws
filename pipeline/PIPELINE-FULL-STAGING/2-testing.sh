99a2f44b85af8990d115edf59d61189f728ba85c
Johnatan Ruiz Campos <je_blue87@hotmail.com>
2021-06-13 09:50:03 +0000

    v1.8.1

--- pipeline/PIPELINE-FULL-STAGING/2-testing.sh
+++ pipeline/PIPELINE-FULL-STAGING/2-testing.sh
@@ -2,24 +2,30 @@
 
 source py37-venv/bin/activate
 set -x
+
+#a) Prueba de Revisión estática de Código con radon
+radon cc src -a -nc
+echo 'Resultado:\n'.$?
+
+#if [[ $? -ne 0 ]]
+#then
+#	exit 1
+#fi
+
+#b) Prueba de Calidad de Código con flake8
 flake8 src
 if [[ $? -ne 0 ]]
 then
 	exit 1
 fi
 
-radon cc src
-if [[ $? -ne 0 ]]
-then
-	exit 1
-fi
-
+#c) Prueba de Seguridad en el Código con bandit
 bandit -r src
 if [[ $? -ne 0 ]]
 then
 	exit 1
 fi
 
-cd test/unified/
-coverage run TestToDoClass.py
-coverage report -m
+cd tests/unit/
+#coverage run TestToDoTableClass.py
+#coverage report -m
